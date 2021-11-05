2c7769b2782b97dbb3c6dfd9c402b030bd2e2e2f
support <support@DESKTOP-648IQJL>
2021-11-05 16:44:30 +0100

    Add project files.

---
+++ TicketDispensingApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TicketDispensingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ticketType = new Ticket();
+            ticketType.ticketNumber();
+            
+      
+        }
+    }
+    class Ticket
+
+    {
+        public int year = 0;
+        public double total = 0; 
+        public double bossMoney;
+        public string ticketType;
+        public void ticketNumber()
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("Please Enter the ticket type either Daily or Monthly");
+            string ticketType = Console.ReadLine();
+            
+
+        }
+        public double Amount()
+        {
+            Console.WriteLine($"Amount accumulate is {total} for {year} years");
+            Console.WriteLine($"You are to remit {bossMoney} to your Boss");
+            return Amount();
+        }
+    }
+}
+
+
+    
+
